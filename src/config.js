1d2bbf95bc5b1bd3ead11d862ceebeed5c304ae6
ckarabeyoglu <95589575+ckarabeyoglu@users.noreply.github.com>
2022-04-11 18:16:45 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x09233d553058c2F42ba751C87816a8E9FaE7Ef10",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "My Pet Hooligan",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://mypethooligan.com/pic/5925.png",
   },
 
   // Supported networks - just for your reference
